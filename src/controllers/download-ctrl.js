458f6b33d73e55d3e72226baec77046b45943c13
Rukaiya Khondaker <rnk23@ic.ac.uk>
2024-09-09 22:33:49 +0100

    Added code to integrate python script into the website by executing server side

--- src/controllers/download-ctrl.js
+++ src/controllers/download-ctrl.js
@@ -1,31 +1,43 @@
 const ytdl = require("@distube/ytdl-core");
 const fs = require("fs");
-import { PoseLandmarker, FilesetResolver } from "@mediapipe/tasks-vision";
+const { spawn } = require("child_process");
 
-let poseLandmarker = undefined;
-let runningMode = "VIDEO";
-
-const createPoseLandmarker = async () => {
-    const vision = await FilesetResolver.forVisionTasks("../../node_modules/@mediapipe/tasks-vision/wasm");
-    poseLandmarker = await PoseLandmarker.createFromOptions(vision, {
-        baseOptions: {
-            modelAssetPath: `/js/pose_landmarker_lite.task`,
-            delegate: "GPU"
-        },
-        runningMode: runningMode,
-        numPoses: 2
+function runPythonPoseSeq(scriptPath, args, callback) {
+    const pythonProcess = spawn('python', [scriptPath].concat(args));
+ 
+    let data = '';
+    pythonProcess.stdout.on('data', (chunk) => {
+        data += chunk.toString(); 
+    });
+ 
+    pythonProcess.stderr.on('data', (error) => {
+        console.error(`stderr: ${error}`);
+    });
+ 
+    pythonProcess.on('close', (code) => {
+        if (code !== 0) {
+            console.log(`Python script exited with code ${code}`);
+            callback(`Error: Script exited with code ${code}`, null);
+        } else {
+            console.log('Python script executed successfully');
+            callback(null, data);
+        }
     });
 }
-
-createPoseLandmarker();
-
-
 
 module.exports.getDownload = (req, res) => {
     var URL = req.query.URL;
     res.send(`Downloaded ${URL}!`);
-    
+
     ytdl(URL, {
         format: "mp4"
     }).pipe(fs.createWriteStream("src/models/videos/video.mp4"));
+
+    runPythonPoseSeq("src/pose-sequence.py", ["src/models/videos/video.mp4"], (err, result) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.send("Pose sequence generation success!");
+        }
+    });
 };
